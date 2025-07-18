b6f57cf9876d9006a229aa74bf9e539b0871dd37
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-07-18 14:39:14 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
 let age: number = 2 // need to mention type of value
+let course: string = "TypeScript Course" // cant be anything other than a string
 
-age = 4 
-
-
-// Static Typing:  mentioning type before you declare
 
