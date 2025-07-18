d92b92e5b914c7a8d6ba0e8094e06ecfe2aa81a8
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-07-18 14:41:36 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
+// BASIC TYPES
 let age: number = 2 // need to mention type of value
 let course: string = "TypeScript Course" // cant be anything other than a string
-
+let lessonCompleted: boolean = true
+let x: any = "134" 
 
