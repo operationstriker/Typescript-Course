1a2b8e4df7d2309acf65d08cf258bdfcd3901285
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-07-21 14:39:38 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+// BASIC TYPES
+let age = 2; // need to mention type of value
+let course = "TypeScript Course"; // cant be anything other than a string
+let lessonCompleted = true;
+let x = false; // any - any variable type
+// array of courses, array of strings
+let courses = ["HTML", "CSS"]; // only strings
+let arr = [true, '123', 321];
+// each element was an array
+let stringArr = [['a'], ['b']]; // array of arrays
